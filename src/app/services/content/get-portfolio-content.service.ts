94334bb91e7b55cd7a8e63ad7871e1aacf445ef8
Marek Gudalewicz <marek.gudalewicz@localhost>
2021-09-25 01:20:39 +0200

    switching component zoom effect, getting over stacking order issue

--- src/app/services/content/get-portfolio-content.service.ts
+++ src/app/services/content/get-portfolio-content.service.ts
@@ -24,39 +24,39 @@ export class GetPortfolioContentService {
               function based calculator
             `
           },
-          // {
-          //   type: 'portfolio-entry',
-          //   imageName: 'storyDices1.JPG',
-          //   imageNameArray: ['storyDices1.JPG', 'StoryDices2.JPG', 'storyDices3.JPG'],
-          //   title: 'Story dices',
-          //   content: `
-          //     Are you a story teller? Draw random symbols and think of a story based on them. You may chose 
-          //     number of dices, and select topics. You may change one dice if the image is not comfortable, 
-          //     of you may redraw all images
-          //   `
-          // },
-          // {
-          //   type: 'portfolio-entry',
-          //   imageName: 'ticTacToe3.JPG',
-          //   imageNameArray: 
-          //     ['ticTacToe3.JPG', 'TicTacToe2.JPG','ticTacToe5.JPG', 'ticTacToe4.JPG', 'TicTacToeAbout.JPG'],
-          //   title: 'Tic-tac-toe',
-          //   content: `
-          //     Play a set of tic-tac-toe game with computer of a friend on the same computer. You may chose to 
-          //     play on bigger boards with more figures in a row, or even see computer playing against its self.
-          //   `
-          // },
-          // {
-          //   type: 'portfolio-entry',
-          //   imageName: 'widgets2.JPG',
-          //   imageNameArray: ['widgets2.JPG', 'widgets1.JPG', 'widgets3.JPG'],
-          //   title: 'Widgets',
-          //   content: `
-          //     A pure JavaScript page with different widgets like animated buttons, spinning circles, gauges or a 
-          //     spinning wheel. You may peek a code snippet, change color or size. Project is available on 
-          //     github <i class="fab fa-github"></i>. Widgets are encapsulated as custom web components.
-          //   `
-          // }
+          {
+            type: 'portfolio-entry',
+            imageName: 'storyDices1.JPG',
+            imageNameArray: ['storyDices1.JPG', 'StoryDices2.JPG', 'storyDices3.JPG'],
+            title: 'Story dices',
+            content: `
+              Are you a story teller? Draw random symbols and think of a story based on them. You may chose 
+              number of dices, and select topics. You may change one dice if the image is not comfortable, 
+              of you may redraw all images
+            `
+          },
+          {
+            type: 'portfolio-entry',
+            imageName: 'ticTacToe3.JPG',
+            imageNameArray: 
+              ['ticTacToe3.JPG', 'TicTacToe2.JPG','ticTacToe5.JPG', 'TicTacToe4.JPG', 'TicTacToeAbout.JPG'],
+            title: 'Tic-tac-toe',
+            content: `
+              Play a set of tic-tac-toe game with computer of a friend on the same computer. You may chose to 
+              play on bigger boards with more figures in a row, or even see computer playing against its self.
+            `
+          },
+          {
+            type: 'portfolio-entry',
+            imageName: 'widgets2.JPG',
+            imageNameArray: ['widgets2.JPG', 'widgets1.JPG', 'widgets3.JPG'],
+            title: 'Widgets',
+            content: `
+              A pure JavaScript page with different widgets like animated buttons, spinning circles, gauges or a 
+              spinning wheel. You may peek a code snippet, change color or size. Project is available on 
+              github <i class="fab fa-github"></i>. Widgets are encapsulated as custom web components.
+            `
+          }
         ]
       },
       
